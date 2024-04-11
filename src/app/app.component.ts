4c12d2101a8450e076821a5cc6165468d0e7e2d8
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-04-11 12:03:42 -0300

    feat: calling request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Question } from "./model/question.model";
+import { QuestionService } from "./services/question.service";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'untitled';
+  questions: Question[] = [];
+
+  constructor(private questionService: QuestionService) {
+    this.getQuestions();
+  }
+
+  //TODO: place function on the right file
+  getQuestions() {
+    //TODO: get category and difficulty dynamically
+    this.questionService.getQuestions("linux", "Easy").subscribe(data => {
+      this.questions = data;
+      //TODO: remove console.log()
+      console.log(this.questions);
+    })
+  }
 }
