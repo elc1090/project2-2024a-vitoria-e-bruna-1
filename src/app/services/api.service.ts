85c16791dcdc240a277ca62f18fce5bb61fe206d
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-04-19 19:09:36 -0300

    refactor: category method  returning Observable

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,10 +1,11 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class apiService {
+export class ApiService {
 
   private apiUrl = 'https://quizapi.io/api/v1';
   private apiKey = 'dHcXKcVlv2wWXiMxg4FE1XGUVyY8oTZwtbPk1xJM'
@@ -12,14 +13,8 @@ export class apiService {
   constructor(private http: HttpClient) {
   }
 
-  getCategories(){
-    return new Promise((resolve, reject) => {
-      return this.http.get(`${this.apiUrl}/categories?apiKey=${this.apiKey}`).subscribe((data: any) => {
-        resolve(data);
-      }, (error: any) => {
-        reject(error);
-      });
-    });
+  getCategories(): Observable<{ name: string }[]> {
+    return this.http.get<{ name: string }[]>(`${this.apiUrl}/categories?apiKey=${this.apiKey}`);
   }
 
   getQuestions(category: string, difficulty: string) {
